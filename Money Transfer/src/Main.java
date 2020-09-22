de11db0c8d1a97038822be71b1210a1f13658463
Julia Kernitckaya <kernyusha@yandex.ru>
2020-09-22 17:00:55 +0300

    add money transfer

---
+++ Money Transfer/src/Main.java
@@ -0,0 +1,10 @@
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        int currentBill = 2000000000;
+        int transitionSum = 500000000 ;
+        int total = currentBill + transitionSum;
+        System.out.println(total);
+    }
+}
